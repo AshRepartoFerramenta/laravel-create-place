a396d2d52ab9138224c05d54db1407206ab0d2a9
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-09-07 09:10:35 +0200

    laravel-create-place

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+  public function index() {
+    $students = Student::all();
+    return view('students', compact('students'));
+  }
+
+  public function handlebars() {
+      return view('studentshandlebars');
+  }
+}
